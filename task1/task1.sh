ea4a2f7836222646f8a537d3e4e219a48735f24b
salierin <salierin@proton.me>
2025-07-31 20:53:00 +0400

    shfmt

--- task1/task1.sh
+++ task1/task1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 show_help() {
-    cat << EOF
+	cat <<EOF
 Usage: ${0##*/} [OPTIONS] [ARGUMENTS...]
 
 Options:
@@ -13,46 +13,44 @@ Examples:
 EOF
 }
 
-
 check_url() {
-  while IFS= read -r line; do
-    local response
+	while IFS= read -r line; do
+		local response
 
-    echo -n "Checking availablitiy for $line... "
-    response=$(curl -s -o /dev/null -w "%{http_code}" "$line")
-    echo "$response"
+		echo -n "Checking availablitiy for $line... "
+		response=$(curl -s -o /dev/null -w "%{http_code}" "$line")
+		echo "$response"
 
-    if [[ ! "$response" =~ [23][0-9]{2} ]]; then
-      exit
-    fi
+		if [[ ! "$response" =~ [23][0-9]{2} ]]; then
+			exit
+		fi
 
-  done < "$1"
+	done <"$1"
 }
 
-
-if [[ "$#" -eq 0 ]]; then 
-  show_help
-  exit 0
+if [[ "$#" -eq 0 ]]; then
+	show_help
+	exit 0
 fi
 
 case "$1" in
-    -h|--help)
-        show_help
-        exit 0
-        ;;
-    -f|--file)
-        if [[ -n "$2" && -f "$2" ]]; then
-            check_url "$2"
-            shift 2
-        else
-            echo "Error: no file provided or file does not exist"
-            show_help
-            exit 1
-        fi
-        ;;
-    *)
-        echo "Error: Unknown option '$1'."
-        show_help
-        exit 1
-        ;;
+-h | --help)
+	show_help
+	exit 0
+	;;
+-f | --file)
+	if [[ -n "$2" && -f "$2" ]]; then
+		check_url "$2"
+		shift 2
+	else
+		echo "Error: no file provided or file does not exist"
+		show_help
+		exit 1
+	fi
+	;;
+*)
+	echo "Error: Unknown option '$1'."
+	show_help
+	exit 1
+	;;
 esac
